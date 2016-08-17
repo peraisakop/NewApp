4a9da1807ccb0dfc29c890b00ede844974f2549e
peraisakop <peralivet@yahoo.com>
2016-08-17 12:38:50 +0100

    Created the Create Function for notes

--- NotesApplication.js
+++ NotesApplication.js
@@ -1,9 +1,17 @@
 class NotesApplication {
 	//constructor with author and note
-	constructor (author, note) {
+	constructor (author) {
         this.author = author;
         this.note = [];
     }
+
+
+    //created the create function
+    create(note_content) {
+    	this.note.push(note_content);
+    }
+
+
          
      
 }
